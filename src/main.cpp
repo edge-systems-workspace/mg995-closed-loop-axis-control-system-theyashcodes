b685538329da8575ed1ad24545754f7c03b331fc
theyashcodes <yash.gupta_cs.aiml24@gla.ac.in>
2026-02-20 20:39:57 +0530

    feat: finalized MG995 control logic and documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
+/**
+* @file main.cpp
+ * @brief Closed-loop control system for MG995 servo motor
+ * @author Yash Gupta
+ * @date 2026-02-20
+ */
+
 #include <Arduino.h>
+#include <Servo.h>
+
+#define SERVO_PIN 9
+#define FEEDBACK_PIN A0
+
+Servo servoMotor;
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    servoMotor.attach(SERVO_PIN);
 }
 
-
 void loop() {
-// write your code here
+    int feedbackValue = analogRead(FEEDBACK_PIN);
+    int targetAngle = map(feedbackValue, 0, 1023, 0, 180);
+
+    servoMotor.write(targetAngle);
+
+    Serial.print("Feedback: ");
+    Serial.print(feedbackValue);
+    Serial.print(" | Angle: ");
+    Serial.println(targetAngle);
+
+    delay(100);
 }
